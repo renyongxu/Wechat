76109da58ae853d748e4d6654429232b3bc32180
yongxu <Ryx123>
2018-03-10 17:30:24 +0800

    new wechat

---
+++ pages/home/home.js
@@ -0,0 +1,89 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      // title: that.data.mername//页面标题为路由参数
+      title : "首页"
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+  // 前往详情页
+  toDetail : function () {
+    wx.navigateTo({
+      url: '../detail/detail',
+    })
+    console.log(1);
+    //  wx.showModal({
+    //    title: '提示',
+    //    content: '单击事件被触发',
+    //    showCancel: false
+    //  })
+  }
+ 
+})
